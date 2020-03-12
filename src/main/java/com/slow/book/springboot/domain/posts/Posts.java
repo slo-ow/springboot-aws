f0aefed5a40ce0046d33e50439769b79c8488c52
slo-ow <icet25@naver.com>
2020-03-12 19:23:40 +0900

    115P 까지 완료. PostsApiController 수정 PostsApiControllerTest 수정 PostsUpdateRequestDto 추가 PostsResponseDto 추가 PostsService 추가

--- src/main/java/com/slow/book/springboot/domain/posts/Posts.java
+++ src/main/java/com/slow/book/springboot/domain/posts/Posts.java
@@ -33,4 +33,9 @@ public class Posts { // 실제 DB와 매칭 될 Class
         this.content = content;
         this.author = author;
     }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }

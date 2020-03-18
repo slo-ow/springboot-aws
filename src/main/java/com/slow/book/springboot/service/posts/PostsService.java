bcd610f920412fa21e3c9fd0bf921d7caa4bc708
slo-ow <61968619+slo-ow@users.noreply.github.com>
2020-03-18 11:17:24 +0900

    fixed untracked files

--- src/main/java/com/slow/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/slow/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.slow.book.springboot.service.posts;
 
 import com.slow.book.springboot.domain.posts.Posts;
 import com.slow.book.springboot.domain.posts.PostsRepository;
+import com.slow.book.springboot.web.dto.PostsListResponseDto;
 import com.slow.book.springboot.web.dto.PostsResponseDto;
 import com.slow.book.springboot.web.dto.PostsSaveRequestDto;
 import com.slow.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import  org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -20,20 +24,39 @@ public class PostsService {
     }
 
     @Transactional
-    public Long update(Long id, PostsUpdateRequestDto requestDto){
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
-        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
-
-        posts.update(requestDto.getTitle(),requestDto.getContent());
+        posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
 
-    public PostsResponseDto findById (Long id){
+    @Transactional
+    public void delete (Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
-        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
+        postsRepository.delete(posts);
+        /*
+            JpaRepository 에서 이미 delete 메소드를 지원하고 있으니 이를 활용한다. Entity 를 파라미터로 삭제할 수 도있고, deleteById 메소드를 이용하면 id 로 삭제할수도있다.
+            존재하는 Posts 인지 확인을 위해 Entity 조회 후 그대로 삭제한다.
+         */
+    }
+
+    @Transactional(readOnly = true)
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
     }
 
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
 }
--- src/main/java/com/slow/book/springboot/web/IndexController.java
+++ src/main/java/com/slow/book/springboot/web/IndexController.java
@@ -1,13 +1,22 @@
 package com.slow.book.springboot.web;
 
+import com.slow.book.springboot.service.posts.PostsService;
+import com.slow.book.springboot.web.dto.PostsResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 
+@RequiredArgsConstructor
 @Controller
 public class IndexController {
 
+    private final PostsService postsService;
+
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) { // 1. 서버 템플릿 엔진에서 사용할 수 있는 객체를 저장할 수 있다, postsService.findAllDesc()로 가져온 결과를 posts 로 index.mustache 에 전달한다.
+        model.addAttribute("posts",postsService.findAllDesc());
         return "index";
         /*
             gradle 의 머스테치 스타터 덕분에 컨트롤러에서 문자열을 반환할 때 앞의 경로와 뒤의 파일 확장자는 자동으로 지정 된다.
@@ -15,4 +24,20 @@ public class IndexController {
             반환 하므로, src/main/resources/templates/index.mustache 로 전환되어 View Resolver 가 처리하게 된다.
          */
     }
+
+    @GetMapping("/posts/save")
+    public String postsSave() {
+
+        return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+
+        PostsResponseDto dto = postsService.findById(id);
+        model.addAttribute("posts",dto);
+
+        return "posts-update";
+    }
+
 }

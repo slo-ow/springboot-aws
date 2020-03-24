7dd7e7b8e749148530f07ab45fecee1311073bbf
slo-ow <61968619+slo-ow@users.noreply.github.com>
2020-03-24 22:46:11 +0900

    1-1. application.properties 안에 Test OAuth 추가 1-2. build.gradle 안에 spring-security-test 추가 1-3. @WithMockUser(roles="USER") 를 이용한 Test 작동 전부 문제 없이 실행 완료

--- src/test/java/com/slow/book/springboot/web/HelloControllerTest.java
+++ src/test/java/com/slow/book/springboot/web/HelloControllerTest.java
@@ -1,8 +1,13 @@
 package com.slow.book.springboot.web;
+
+import com.slow.book.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -12,14 +17,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+                @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+        }
+)
 // 1-1. 테스트를 진행할 때 JUnit 에 내장된 실행자 외에 다른 실행자를 실행시킨다. 여기서는 SpringRunner 라는 스프링 실행자를 사용한다.
-@RunWith(SpringRunner.class) // 1-2. 즉, 스프링 부트 테스트와 Junit 사이에 연결자 역할을 한다.
-@WebMvcTest(controllers = HelloController.class) // 2-1. Web(SpringMVC)에 집중할 수 있는 어노테이션, @Controller, @ControllerAdvice 등을 사용할 수 있음.
+// @RunWith(SpringRunner.class) // 1-2. 즉, 스프링 부트 테스트와 Junit 사이에 연결자 역할을 한다.
+// @WebMvcTest(controllers = HelloController.class) // 2-1. Web(SpringMVC)에 집중할 수 있는 어노테이션, @Controller, @ControllerAdvice 등을 사용할 수 있음.
 public class HelloControllerTest {               // 2-2. 단, @Service, @Component, @Repository 등은 사용할 수 없음.
 
     @Autowired // 3. 스프링이 관리하는 빈(Bean)을 주입 받는다.
     private MockMvc mvc; // 4. 웹 API 를 테스트할 때 사용한다. 스프링 MVC 테스트의 시작점이다. 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있다.
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -29,6 +41,7 @@ public class HelloControllerTest {               // 2-2. 단, @Service, @Compone
                 .andExpect(content().string(hello)); // 7. mvc.perform 의 결과를 검증. 응답 본문의 내용을 검증함. Controller 에서 "hello" 를 리턴하기 때문에 이 값을 검증함.
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception{
         String name = "hello";
